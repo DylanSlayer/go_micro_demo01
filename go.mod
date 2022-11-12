6b44bdcbd3eaf3867a217994c8ed0408e81dbee3
slay <1771391789@qq.com>
2022-11-12 14:03:12 +0800

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/DylanSlayer/go_demo01
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("hello world")
+	timeNow := time.Now()
+	fmt.Println(timeNow)
+}
